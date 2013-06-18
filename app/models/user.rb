f45087d43c1b636d48f2d035fd4551eaad3611ec
haul <h4ul@me.com>
2013-06-18 14:25:33 +0400

    add user.email and user.password_digest (throw has_secure_password) presence validation and specs for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :fullname, :password_digest
+  attr_accessible :email, :fullname, :password, :password_confirmation
+
+  has_secure_password
+
+  validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:user) { Fabricate.build(:user)}
+  subject { user }
+
+  it { should respond_to(:email) } 
+  it { should respond_to(:fullname) } 
+  it { should respond_to(:password_digest) }
+
+  it { should be_valid }
+  
+  describe "when email is not present" do
+    before { user.email = "" }
+    it {should_not be_valid}
+  end
+
+  describe "when password_digest is not present" do 
+    before { user.password_digest = "" }
+    it { should_not be_valid }
+  end
 end
