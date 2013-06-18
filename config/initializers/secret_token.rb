e3d6ead369564ec0896704c7e4940998113c02f1
haul <h4ul@me.com>
2013-06-18 06:11:41 +0400

    install rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyHappyDay::Application.config.secret_token = '0b9b6f78a30f570ef6fe2b5174a3177dae2ba5e0da9c9c8c15b24c4098ad185ba640358dd666667a892d06fe468774e8e302eb3316976333ba92ae1f844f340b'
